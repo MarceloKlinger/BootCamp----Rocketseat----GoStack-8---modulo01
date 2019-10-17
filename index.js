36e9a35af18191e990aad0270f0369e8a4b32d52
Marcelo Klinger <marcelo.klinger@hotmail.com>
2019-10-17 09:40:54 -0300

    express

--- index.js
+++ index.js
@@ -2,10 +2,33 @@ const express = require('express');
 
 const server = express();
 
+
+// express ler arquivos json
+server.use(express.json());
+
 // Query params = ?teste=1
-// Route params = /users/1
+// Route params = /users/1 = localhost:3000/users/12341
 // Request body = { "name": "Marcelo", "email": "marcelo.klinger@hotmail.com" }
 
-server.get('/teste', (req, res) => res.json({ message: 'Hello World' }));
+// CRUD = Create, Read, Update, Delete
+
+const users = ['Marcelo', 'Diego', 'José'];
+
+server.get('/users', (req, res) => res.json(users));
+
+server.get('/users/:index', (req, res) => {
+  // eslint-disable-next-line prefer-destructuring
+  const { index } = req.params;
+
+  return res.json(users[index]);
+});
+
+server.post('/users', (req, res) => {
+  const { name } = req.body;
+
+  users.push(name);
+
+  return res.json(users);
+});
 
 server.listen(3000);
